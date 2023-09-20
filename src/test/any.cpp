57bb6cc47092a0c9cca13400ef9ac77becc3908c
James Chan <jameschan312.cn@gmail.com>
2023-09-20 17:47:48 -0400

    Sep 20, 2023

--- src/test/any.cpp
+++ src/test/any.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-int findDifferentElement(const vector<int> &a, const vector<int> &b) {
+int findAdditionalElement(const vector<int> &a, const vector<int> &b) {
     // How to implement this function?
     return 0;
 }
 
 int main() {
-    int result = findDifferentElement(
+    int result = findAdditionalElement(
         vector{1, 4, 9, 9, 3},
         vector{9, 3, 4, 5, 9, 1}
-        );
+    );
     std::cout << result << std::endl;   // expect: "5"
 
     return 0;
