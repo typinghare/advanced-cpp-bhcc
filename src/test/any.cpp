fe82686edf79a35707a8a04bcc5b74e290034d0d
James Chan <jameschan312.cn@gmail.com>
2023-10-21 23:44:18 -0400

    Oct 21, 2023

--- src/test/any.cpp
+++ src/test/any.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-class Outer {
-private:
-    int foo;
-public:
-    explicit Outer(int foo) : foo(foo) {}
+// Normal variable
+constexpr double E = 2.71828;
 
-    class Inner {
-    public:
-        int bar(const Outer &outer) {
-            // Access outer class private properties
-            return outer.foo * 2;
-        }
-    };
-};
+// Variable templates
+template<typename T>
+constexpr T PI{T{3.1415926}};
 
 int main() {
-    Outer outer{5};
-    Outer::Inner inner;
+    // Using variable templates
+    double pi{PI<double>};
+    double circumference = 3 * pi;
 
-    std::cout << inner.bar(outer) << std::endl;
+    // Using static_cast
+    double num1 = 2 * static_cast<float>(E);
+
+    // Using (float)
+    float num2 = 2 * (float) E;
+
+    std::cout << circumference << std::endl;
+    std::cout << num1 << std::endl;
+    std::cout << num2 << std::endl;
 
     return 0;
 }
+
 
